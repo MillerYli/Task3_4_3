0b8b6a51ebc928595eb6fd8e823fa8b8a9d91491
Yulia Miller <miller82.98@mail.ru>
2022-12-05 13:21:32 -0800

    Добавьте файлы проекта.

---
+++ Task3_4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    enum Semaphore: int
+    {
+        red = 100,
+        yellow = 200,
+        green = 300
+    }
+}
